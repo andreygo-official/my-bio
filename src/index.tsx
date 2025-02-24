8d11664276422c9a989316cead0ebcdf09cb8f33
strafelord24@gmail.com <andriihorobets@Andriis-MacBook-Pro.local>
2025-02-24 13:05:44 +0200

    fixed deployment problems

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
